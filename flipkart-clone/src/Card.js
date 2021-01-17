2cd949b25b03763d4470c396573b15e822412d19
shubham.snu@gmail.com <shubham.snu@gmail.com>
2021-01-17 16:55:09 +0530

    refactor: attept for add to cart

--- flipkart-clone/src/Card.js
+++ flipkart-clone/src/Card.js
@@ -1,15 +1,31 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import "./Card.css";
+import { addItem } from "./features/cartSlice";
 function Card({ id, name, type, price, discountPercentage, imgUrl }) {
   const history = useHistory();
-  const goToDetails = () => {
-    history.push({
-      pathname: `/product/${id}`,
-    });
+  const dispatch = useDispatch();
+  // const goToDetails = () => {
+  //   history.push({
+  //     pathname: `/product/${id}`,
+  //   });
+  // };
+  const addToCart = () => {
+    dispatch(
+      addItem({
+        id,
+        name,
+        type,
+        price,
+        discountPercentage,
+        imgUrl,
+      })
+    );
+    history.push("/cart");
   };
   return (
-    <div className="card" onClick={goToDetails}>
+    <div className="card">
       <img src={imgUrl} alt={name} className="card--image" />
       <h5>{type}</h5>
       <h6>{name}</h6>
@@ -17,6 +33,7 @@ function Card({ id, name, type, price, discountPercentage, imgUrl }) {
         <p>{price}</p>
         <span>{discountPercentage}</span>
       </div>
+      <button onClick={addToCart}>Add to Card</button>
     </div>
   );
 }
